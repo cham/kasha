1fe21f00a98350e8294c6e19a53e4f385d4ccadf
Dan Neame <danneame@gmail.com>
2014-07-13 16:07:19 +0100

    Removed unused functionality e.g.: public and views

--- server.js
+++ server.js
@@ -6,42 +6,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
-
-var indexRoutes = require('./routes/index');
 var subscriptionsRoutes = require('./routes/subscriptions');
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('hogan-express'));
-app.set('view engine', 'html');
-//app.set('view engine', 'jade');
 
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRoutes);
 app.use('/subscriptions', subscriptionsRoutes);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-app.use(function(err, req, res) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 app.set('port', process.env.PORT || 3000);
 
