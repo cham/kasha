36386e313d1bba9379d8e2bf416ec390beafbdbd
Dan Neame <danneame@gmail.com>
2014-07-13 11:37:27 +0100

    Added clustering

--- app.js
+++ app.js
@@ -1,62 +1,26 @@
 'use strict';
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var cluster   = require('cluster'),
+    client = require('redis').createClient();
 
-var indexRoutes = require('./routes/index');
-var subscriptionsRoutes = require('./routes/subscriptions');
+client.set('redis-connection-test', 'ok', function(err){
+    if(err){
+        return console.log(err);
+    }
 
-var app = express();
+    var cpuCount = require('os').cpus().length;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('hogan-express'));
-app.set('view engine', 'html');
-//app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRoutes);
-app.use('/subscriptions', subscriptionsRoutes);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+    cluster.setupMaster({
+        exec : 'worker.js'
     });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+    for(var i = 0; i < cpuCount; i += 1){
+        cluster.fork();
+    }
+
+    cluster.on('exit', function(worker){
+        console.log('Worker ' + worker.id + ' died');
+        cluster.fork();
     });
+
+    console.log('Master process started');
 });
-
-
-module.exports = app;
